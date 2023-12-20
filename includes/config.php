2c797090d95baa856075b1c69e927cc0e6cbf673
Cassiano Castro <development.castro@gmail.com>
2023-12-20 17:36:07 -0300

    Indent adjust

--- includes/config.php
+++ includes/config.php
@@ -1,16 +1,23 @@
 <?php
+error_reporting(E_ALL);
+ini_set("display_errors", true);
 
-  $db_conn = mysqli_connect('localhost', 'root', 'root','sms_project');
+$db_conn = mysqli_connect('localhost', 'php', 'php', 'sms_project');
 
-  if (!$db_conn) {
+if ( ! $db_conn )
+{
     echo 'Connection Failed';
+
     exit;
-  }
-  session_start();
-  // if(empty($_SESSION) || !isset($_SESSION['login']))
-  // {
-  //   session_start();
-  // }
-  date_default_timezone_set('Asia/Kolkata');
-  include('functions.php');
-?>
+}
+
+session_start();
+
+// if ( empty($_SESSION) || ! isset($_SESSION['login']) )
+// {
+//   session_start();
+// }
+
+date_default_timezone_set('Asia/Kolkata');
+
+require 'functions.php';
