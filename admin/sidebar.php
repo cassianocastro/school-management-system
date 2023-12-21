e4340f0ef34aebb2e5365066eb75590f1bb10b9d
Cassiano Castro <development.castro@gmail.com>
2023-12-21 20:00:04 -0300

    Path adjust

--- admin/sidebar.php
+++ admin/sidebar.php
@@ -289,7 +289,7 @@
           </a>
           <ul class="nav nav-treeview">
             <li class="nav-item">
-              <a href="<?=$site_url?>/admin/acadamy-settings/basic-information.php" class="nav-link">
+              <a href="<?=$site_url?>/admin/academy-settings/basic-information.php" class="nav-link">
                 <i class="far fa-circle nav-icon"></i>
                 <p>Basic Informations</p>
               </a>
