37bcd0c93cff6cdd598f85d9f63d75dafdcaddfe
Cassiano Castro <development.castro@gmail.com>
2023-12-23 16:51:27 -0300

    Moved inline script to ./index.js

--- teacher/footer.php
+++ teacher/footer.php
@@ -28,29 +28,6 @@
 <script defer src="../plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
 <script defer src="../dist/js/adminlte.js"></script>
 <script defer src="../dist/js/demo.js"></script>
-<script>
-  (function () {
-    var path = window.location.href;
-    // console.log(path);
-
-    $(".nav-link").each(function () {
-      var href = $(this).attr('href');
-      // console.log(href);
-
-      if ( path === decodeURIComponent(href) )
-      {
-        $(this).addClass('active');
-
-        var parent = $(this).closest('.has-treeview');
-
-        parent.addClass('menu-open');
-
-        $(parent).find('.nav-link').first().addClass('active');
-
-        // console.log(parent);
-      }
-    });
-  }());
-</script>
+<script defer src="./index.js"></script>
 </body>
 </html>
