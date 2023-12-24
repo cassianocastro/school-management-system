fce517457399c74f781cc68166482fe13d29cec6
Cassiano Castro <development.castro@gmail.com>
2023-12-24 14:45:27 -0300

    Removed script tags for framework's inclusions

--- teacher/footer.php
+++ teacher/footer.php
@@ -1,33 +1,13 @@
+<footer class="main-footer">
+  <strong>
+    Copyright &copy; 2020
+
+    <a rel="external" type="text/html" hreflang="en" href="https://technostudy.co.in">School Managment System</a>.
+  </strong>
+
+  All rights reserved.
+
+  <div class="float-right d-none d-sm-inline-block">
+    <b>Version</b> 1.0
   </div>
-  <!-- Main Footer -->
-  <footer class="main-footer">
-    <strong>
-      Copyright &copy; 2020
-
-      <a rel="external" type="text/html" hreflang="en" href="https://technostudy.co.in">School Managment System</a>.
-    </strong>
-
-    All rights reserved.
-
-    <div class="float-right d-none d-sm-inline-block">
-      <b>Version</b> 1.0
-    </div>
-  </footer>
-</div>
-<!-- ./wrapper -->
-
-<!--
-  1. jQuery
-  2. Bootstrap
-  3. overlayScrollbars
-  4. AdminLTE App
-  5. OPTIONAL SCRIPTS
--->
-<script defer src="../plugins/jquery/jquery.min.js"></script>
-<script defer src="../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-<script defer src="../plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
-<script defer src="../dist/js/adminlte.js"></script>
-<script defer src="../dist/js/demo.js"></script>
-<script defer src="./index.js"></script>
-</body>
-</html>
+</footer>
