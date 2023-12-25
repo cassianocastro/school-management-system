bf5547957ddef56655021dc2b9520c4b7e1a7ffb
Cassiano Castro <development.castro@gmail.com>
2023-12-25 17:32:25 -0300

    Formatting adjust and added title, rel, type, hreflang attributes for the anchor html element

--- student/footer.php
+++ student/footer.php
@@ -1,14 +1,19 @@
   </div>
   <!-- Main Footer -->
   <footer class="main-footer">
-    <strong>Copyright &copy; 2020 <a href="https://technostudy.co.in">School Managment System</a>.</strong>
+    <strong>
+      Copyright &copy; 2020
+
+      <a title="School SysManager" rel="external" type="text/html" hreflang="en" href="https://technostudy.co.in">School Management System</a>.
+    </strong>
+
     All rights reserved.
+
     <div class="float-right d-none d-sm-inline-block">
       <b>Version</b> 1.0
     </div>
   </footer>
-</div>
-<!-- ./wrapper -->
+</div><!-- ./wrapper -->
 
 <!--
   1. Bootstrap
