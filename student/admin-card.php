190066f9992fb7f6a8e27eb44d709d902ca9cf65
Cassiano Castro <development.castro@gmail.com>
2023-12-25 16:15:09 -0300

    Formatting adjust and removed over-commenting

--- student/admin-card.php
+++ student/admin-card.php
@@ -2,33 +2,35 @@
 <?php require_once __DIR__ . '/header.php'; ?>
 <?php require_once __DIR__ . '/sidebar.php'; ?>
 
-    <!-- Content Header (Page header) -->
-    <div class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1 class="m-0 text-dark">Time Table</h1>
-          </div><!-- /.col -->
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Student</a></li>
-              <li class="breadcrumb-item active">Time Table</li>
-            </ol>
-          </div><!-- /.col -->
-        </div><!-- /.row -->
-      </div><!-- /.container-fluid -->
+<!-- Content Header (Page header) -->
+<div class="content-header">
+  <div class="container-fluid">
+    <div class="row mb-2">
+
+      <div class="col-sm-6">
+        <h1 class="m-0 text-dark">Time Table</h1>
+      </div>
+
+      <div class="col-sm-6">
+        <ol class="breadcrumb float-sm-right">
+          <li class="breadcrumb-item"><a href="#">Student</a></li>
+          <li class="breadcrumb-item active">Time Table</li>
+        </ol>
+      </div>
+
     </div>
-    <!-- /.content-header -->
-    <!-- Main content -->
-    <section class="content">
-      <div class="container-fluid">
-        <div class="card">
-            <div class="card-body">
+  </div>
+</div>
 
-            </div>
-        </div>
+<!-- Main content -->
+<section class="content">
+  <div class="container-fluid">
+    <div class="card">
+      <div class="card-body">
 
-      </div><!--/. container-fluid -->
-    </section>
-    <!-- /.content -->
+      </div>
+    </div>
+  </div>
+</section>
+
 <?php require_once __DIR__ . '/footer.php'; ?>
