5ca0582f04ad70a3f626934829706bb8b7174a9f
Cassiano Castro <development.castro@gmail.com>
2023-12-25 15:59:59 -0300

    Moved inline javascript for student/index.js

--- student/footer.php
+++ student/footer.php
@@ -20,31 +20,6 @@
 <script src="../plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
 <script src="../dist/js/adminlte.js"></script>
 <script src="../dist/js/demo.js"></script>
-
-<script>
-  (function(){
-    var path = window.location.href;
-    // console.log(path);
-
-    $(".nav-link").each(function () {
-
-      var href = $(this).attr('href');
-      // console.log(href);
-
-      if ( path === decodeURIComponent(href) )
-      {
-        $(this).addClass('active');
-
-        var parent = $(this).closest('.has-treeview');
-
-        parent.addClass('menu-open');
-
-        $(parent).find('.nav-link').first().addClass('active');
-
-        // console.log(parent);
-      }
-    });
-  }());
-</script>
+<script src="./index.js"></script>
 </body>
 </html>
