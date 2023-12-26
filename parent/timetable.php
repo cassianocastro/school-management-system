f19013b9836a2e1fca2131062b243137d61a0f02
Cassiano Castro <development.castro@gmail.com>
2023-12-26 15:26:52 -0300

    SQL formatting

--- parent/timetable.php
+++ parent/timetable.php
@@ -61,7 +61,37 @@ require_once __DIR__ . '/sidebar.php';
                 $days = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
 
                 foreach ( $days as $day ) :
-                  $query = mysqli_query($db_conn, "SELECT * FROM posts as p INNER JOIN metadata as mc ON (mc.item_id = p.id) INNER JOIN metadata as md ON (md.item_id = p.id) INNER JOIN metadata as mp ON (mp.item_id = p.id) WHERE p.type = 'timetable' AND p.status = 'publish' AND md.meta_key = 'day_name' AND md.meta_value = '$day' AND mp.meta_key = 'period_id' AND mp.meta_value = $period->id AND mc.meta_key = 'class_id' AND mc.meta_value = 1");
+                  $query = mysqli_query(
+                    $db_conn,
+                    <<<SQL
+                      SELECT
+                        *
+                      FROM
+                        posts AS p
+                      INNER JOIN
+                        metadata AS mc ON (mc.item_id = p.id)
+                      INNER JOIN
+                        metadata AS md ON (md.item_id = p.id)
+                      INNER JOIN
+                        metadata AS mp ON (mp.item_id = p.id)
+                      WHERE
+                        p.type = 'timetable'
+                      AND
+                        p.status = 'publish'
+                      AND
+                        md.meta_key = 'day_name'
+                      AND
+                        md.meta_value = '$day'
+                      AND
+                        mp.meta_key = 'period_id'
+                      AND
+                        mp.meta_value = {$period->id}
+                      AND
+                        mc.meta_key = 'class_id'
+                      AND
+                        mc.meta_value = 1
+                    SQL
+                  );
 
                   if ( mysqli_num_rows($query) > 0 ) :
                     while ( $timetable = mysqli_fetch_object($query) ) :
@@ -80,7 +110,7 @@ require_once __DIR__ . '/sidebar.php';
                           <?php
                           $subject_id = get_metadata($timetable->item_id, 'subject_id')[0]->meta_value;
 
-                          echo get_post(array('id' => $subject_id))->title;
+                          echo get_post(['id' => $subject_id])->title;
                           ?>
                         </p>
                       </td>
