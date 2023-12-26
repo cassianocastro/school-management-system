15e492ea0f403b5cc17d557ff045fe08aed56d76
Cassiano Castro <development.castro@gmail.com>
2023-12-26 17:10:05 -0300

    Add file

---
+++ actions/parent.php
@@ -0,0 +1,21 @@
+<?php
+// session_start();
+
+$site_url = "http://{$_SERVER['SERVER_NAME']}/";
+
+if ( isset($_SESSION['login']) )
+{
+  if ( isset($_SESSION['user_type']) and $_SESSION['user_type'] != 'parent' )
+  {
+    $user_type = $_SESSION['user_type'];
+
+    header("Location: /sms-project/$user_type/dashboard.php");
+  }
+}
+else
+{
+  header("Location: ../login.php");
+}
+
+$std_id = $_SESSION['user_id'];
+$parent = get_user_data($std_id);
