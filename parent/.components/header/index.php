aeed3b13769d13dc15583303a85e3cb8d1da9c58
Cassiano Castro <development.castro@gmail.com>
2023-12-26 17:11:34 -0300

    Moved php script to actions/parent.php

--- parent/.components/header/index.php
+++ parent/.components/header/index.php
@@ -1,26 +1,3 @@
-<?php
-// session_start();
-
-$site_url = "http://{$_SERVER['SERVER_NAME']}/";
-
-if ( isset($_SESSION['login']) )
-{
-  if ( isset($_SESSION['user_type']) and $_SESSION['user_type'] != 'parent' )
-  {
-    $user_type = $_SESSION['user_type'];
-
-    header("Location: /sms-project/$user_type/dashboard.php");
-  }
-}
-else
-{
-  header("Location: ../login.php");
-}
-
-$std_id = $_SESSION['user_id'];
-$parent = get_user_data($std_id);
-?>
-
 <nav class="main-header navbar navbar-expand navbar-white navbar-light">
 
   <!-- Left navbar links -->
