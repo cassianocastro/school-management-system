82a4ffdf83a937245626d922ec5e17968b214f35
Cassiano Castro <development.castro@gmail.com>
2023-12-26 13:57:11 -0300

    Changed URL of linked resources and formatting adjust

--- home/.components/achievements/index.php
+++ home/.components/achievements/index.php
@@ -1,4 +1,4 @@
-<section id="achievements" class="py-5 text-white" style="background:#3923a7">
+<section id="achievements" class="py-5 text-white" style="background: #3923a7">
   <div>
     <div class="container">
       <div class="row">
@@ -6,9 +6,11 @@
         <div class="col-lg-6 pr-5">
           <h2>Achievements</h2>
 
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, commodi laboriosam. Ullam aliquam dicta officiis accusamus.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum, commodi laboriosam. Ullam aliquam dicta officiis accusamus.
+          </p>
 
-          <img src="assets/images/still-life-851328_1280.jpg" alt="" class="img-fluid rounded">
+          <img src="assets/img/still-life-851328_1280.jpg" alt="" class="img-fluid rounded">
         </div>
 
         <div class="col-lg-6 my-auto">
@@ -16,9 +18,14 @@
             <div class="col-lg-6 mb-4">
               <div class="border rounded">
                 <div class="card-body text-center">
-                  <span><i class=" text-warning fas fa-graduation-cap fa-2x"></i></span>
+                  <span>
+                    <i class="text-warning fas fa-graduation-cap fa-2x"></i>
+                  </span>
+
                   <h2 class="my-2 font-weight-bold h1">334</h2>
+
                   <hr class="border-warning">
+
                   <h4>Graduates</h4>
                 </div>
               </div>
@@ -27,9 +34,14 @@
             <div class="col-lg-6 mb-4">
               <div class="border rounded">
                 <div class="card-body text-center">
-                  <span><i class=" text-warning fas fa-graduation-cap fa-2x"></i></span>
+                  <span>
+                    <i class="text-warning fas fa-graduation-cap fa-2x"></i>
+                  </span>
+
                   <h2 class="my-2 font-weight-bold h1">334</h2>
+
                   <hr class="border-warning">
+
                   <h4>Graduates</h4>
                 </div>
               </div>
@@ -38,9 +50,14 @@
             <div class="col-lg-6 mb-4">
               <div class="border rounded">
                 <div class="card-body text-center">
-                  <span><i class=" text-warning fas fa-graduation-cap fa-2x"></i></span>
+                  <span>
+                    <i class="text-warning fas fa-graduation-cap fa-2x"></i>
+                  </span>
+
                   <h2 class="my-2 font-weight-bold h1">334</h2>
+
                   <hr class="border-warning">
+
                   <h4>Graduates</h4>
                 </div>
               </div>
@@ -49,9 +66,14 @@
             <div class="col-lg-6 mb-4">
               <div class="border rounded">
                 <div class="card-body text-center">
-                  <span><i class=" text-warning fas fa-graduation-cap fa-2x"></i></span>
+                  <span>
+                    <i class="text-warning fas fa-graduation-cap fa-2x"></i>
+                  </span>
+
                   <h2 class="my-2 font-weight-bold h1">334</h2>
+
                   <hr class="border-warning">
+
                   <h4>Graduates</h4>
                 </div>
               </div>
