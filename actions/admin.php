ceefd7f8371c625d84be10b7a8897768cae496f3
Cassiano Castro <development.castro@gmail.com>
2023-12-27 16:22:45 -0300

    Add file

---
+++ actions/admin.php
@@ -0,0 +1,25 @@
+<?php
+require_once __DIR__ . '/../includes/config.php';
+
+//session_start();
+
+$site_url = "http://{$_SERVER['SERVER_NAME']}";
+
+if ( isset($_SESSION['login']) and $_SESSION['login'] == true )
+{
+  if ( isset($_SESSION['user_type']) and $_SESSION['user_type'] != 'admin' )
+  {
+    $user_type = $_SESSION['user_type'];
+
+    if ( $user_type === "teacher" )
+      header("Location: ../teacher");
+    else
+      header("Location: ../$user_type/dashboard.php");
+  }
+}
+else
+{
+  header("Location: ../login");
+}
+
+require_once __DIR__ . '/dashboard.php';
