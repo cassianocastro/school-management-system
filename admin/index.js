649e22c802d1dd98dea3922fe52f4c1d8b3fe903
Cassiano Castro <development.castro@gmail.com>
2023-12-27 16:46:29 -0300

    Added strict mode and replaced "jQuery()" by ""

--- admin/index.js
+++ admin/index.js
@@ -1,4 +1,33 @@
-(function () {
+"use strict";
+
+$('#class').change(() => {
+    // alert($(this).val());
+
+    $.ajax({
+        url: 'ajax.php',
+        type: 'POST',
+        data: { 'class_id': $(this).val() },
+        dataType: 'json',
+        success: (response) => {
+            if ( response.count > 0 )
+            {
+                $('#section-container').show();
+            }
+            else
+            {
+                $('#section-container').hide();
+            }
+
+            $('#section').html(response.options);
+        }
+    });
+});
+
+/**
+ *
+ */
+function index()
+{
     var path = window.location.href;
 
     // console.log(path);
@@ -20,29 +49,6 @@
             // console.log(parent);
         }
     });
-}());
+}
 
-jQuery(document).ready(() => {
-    jQuery('#class').change(() => {
-        // alert(jQuery(this).val());
-
-        jQuery.ajax({
-            url: 'ajax.php',
-            type: 'POST',
-            data: { 'class_id': jQuery(this).val() },
-            dataType: 'json',
-            success: function (response) {
-                if ( response.count > 0 )
-                {
-                    jQuery('#section-container').show();
-                }
-                else
-                {
-                    jQuery('#section-container').hide();
-                }
-
-                jQuery('#section').html(response.options);
-            }
-        });
-    });
-});
+index();
