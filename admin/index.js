c5e5abf31780e3b542aa151e4b5487452be44171
Cassiano Castro <development.castro@gmail.com>
2023-12-27 16:29:36 -0300

    Add file

---
+++ admin/index.js
@@ -0,0 +1,48 @@
+(function () {
+    var path = window.location.href;
+
+    // console.log(path);
+
+    $(".nav-link").each(function () {
+        var href = $(this).attr('href');
+        // console.log(href);
+
+        if ( path === decodeURIComponent(href) )
+        {
+            $(this).addClass('active');
+
+            var parent = $(this).closest('.has-treeview');
+
+            parent.addClass('menu-open');
+
+            $(parent).find('.nav-link').first().addClass('active');
+
+            // console.log(parent);
+        }
+    });
+}());
+
+jQuery(document).ready(function () {
+    jQuery('#class').change(function () {
+        // alert(jQuery(this).val());
+
+        jQuery.ajax({
+            url: 'ajax.php',
+            type: 'POST',
+            data: { 'class_id': jQuery(this).val() },
+            dataType: 'json',
+            success: function (response) {
+                if ( response.count > 0 )
+                {
+                    jQuery('#section-container').show();
+                }
+                else
+                {
+                    jQuery('#section-container').hide();
+                }
+
+                jQuery('#section').html(response.options);
+            }
+        });
+    });
+});
