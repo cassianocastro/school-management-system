3a1162de5e78d4e72b86feb2454533abd04a3f0c
Cassiano Castro <development.castro@gmail.com>
2023-12-26 22:38:43 -0300

    Added inclusion of the action/teacher.php

--- teacher/.components/routines/.components/timetable/index.php
+++ teacher/.components/routines/.components/timetable/index.php
@@ -1,27 +1,9 @@
 <?php
-require_once __DIR__ . '/../../../../../includes/config.php';
-
 error_reporting(E_ALL);
 ini_set("display_errors", true);
 
-$site_url = "http://{$_SERVER['SERVER_NAME']}";
-
-if ( isset($_SESSION['login']) )
-{
-  if ( isset($_SESSION['user_type']) and $_SESSION['user_type'] != 'teacher' )
-  {
-    $user_type = $_SESSION['user_type'];
-
-    header("Location: ../$user_type/dashboard.php");
-  }
-}
-else
-{
-  header("Location: ../login");
-}
-
-$std_id  = $_SESSION['user_id'];
-$student = get_user_data($std_id);
+require_once __DIR__ . '/../../../../../includes/config.php';
+require_once __DIR__ . '/../../../../../actions/teacher.php';
 ?>
 
 <!DOCTYPE html>
