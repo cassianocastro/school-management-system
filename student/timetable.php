966adcfbea782c9293c6ce563d3bc7b3800ee0ad
Cassiano Castro <development.castro@gmail.com>
2023-12-28 14:16:43 -0300

    Formatting of SQL string

--- student/timetable.php
+++ student/timetable.php
@@ -57,7 +57,37 @@ require_once __DIR__ . '/sidebar.php';
                 $days = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
 
                 foreach ( $days as $day ) :
-                  $query = mysqli_query($db_conn, "SELECT * FROM posts as p INNER JOIN metadata as mc ON (mc.item_id = p.id) INNER JOIN metadata as md ON (md.item_id = p.id) INNER JOIN metadata as mp ON (mp.item_id = p.id) WHERE p.type = 'timetable' AND p.status = 'publish' AND md.meta_key = 'day_name' AND md.meta_value = '$day' AND mp.meta_key = 'period_id' AND mp.meta_value = $period->id AND mc.meta_key = 'class_id' AND mc.meta_value = 1");
+                  $query = mysqli_query(
+                    $db_conn,
+                    <<<SQL
+                      SELECT
+                        *
+                      FROM
+                        posts AS p
+                      INNER JOIN
+                        metadata AS mc ON (mc.item_id = p.id)
+                      INNER JOIN
+                        metadata AS md ON (md.item_id = p.id)
+                      INNER JOIN
+                        metadata AS mp ON (mp.item_id = p.id)
+                      WHERE
+                        p.type = 'timetable'
+                      AND
+                        p.status = 'publish'
+                      AND
+                        md.meta_key = 'day_name'
+                      AND
+                        md.meta_value = '$day'
+                      AND
+                        mp.meta_key = 'period_id'
+                      AND
+                        mp.meta_value = {$period->id}
+                      AND
+                        mc.meta_key = 'class_id'
+                      AND
+                        mc.meta_value = 1
+                    SQL
+                  );
 
                   if ( mysqli_num_rows($query) > 0 ) :
                     while ( $timetable = mysqli_fetch_object($query) ) :
