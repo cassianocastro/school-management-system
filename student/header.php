508d34e9530b85aa3de8838c62d0988aa0f4fc84
Cassiano Castro <development.castro@gmail.com>
2023-12-28 15:48:11 -0300

    Moved PHP script to actions/student.php

--- student/header.php
+++ student/header.php
@@ -1,28 +1,3 @@
-<?php
-// session_start();
-
-// $site_url = "http://localhost/sms-project/";
-$site_url = "http://{$_SERVER["SERVER_NAME"]}/";
-
-if ( isset($_SESSION['login']) )
-{
-  if ( isset($_SESSION['user_type']) and $_SESSION['user_type'] != 'student' )
-  {
-    $user_type = $_SESSION['user_type'];
-
-    header("Location: /sms-project/$user_type/dashboard.php");
-  }
-}
-else
-{
-  header("Location: ../login.php");
-}
-
-$std_id  = $_SESSION['user_id'];
-$student = get_user_data($std_id);
-$stdmeta = get_user_metadata($std_id);
-?>
-
 <nav class="main-header navbar navbar-expand navbar-white navbar-light">
 
   <!-- Left navbar links -->
