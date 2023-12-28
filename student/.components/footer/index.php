ddcbf27197946694f25263388e134f972fd0ced3
Cassiano Castro <development.castro@gmail.com>
2023-12-28 16:22:20 -0300

    Renamed files

---
+++ student/.components/footer/index.php
@@ -0,0 +1,15 @@
+<footer class="main-footer">
+  <strong>
+    Copyright &copy; 2020
+
+    <a title="School SysManager" rel="external" type="text/html" hreflang="en" href="https://technostudy.co.in">
+      School Management System
+    </a>.
+  </strong>
+
+  All rights reserved.
+
+  <div class="float-right d-none d-sm-inline-block">
+    <b>Version</b> 1.0
+  </div>
+</footer>
