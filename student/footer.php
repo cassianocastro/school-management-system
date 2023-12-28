4b1de782823acf9b7f33a677ead9212d58092a1b
Cassiano Castro <development.castro@gmail.com>
2023-12-28 15:43:28 -0300

    Removed HTML script elements

--- student/footer.php
+++ student/footer.php
@@ -1,32 +1,15 @@
+<footer class="main-footer">
+  <strong>
+    Copyright &copy; 2020
+
+    <a title="School SysManager" rel="external" type="text/html" hreflang="en" href="https://technostudy.co.in">
+      School Management System
+    </a>.
+  </strong>
+
+  All rights reserved.
+
+  <div class="float-right d-none d-sm-inline-block">
+    <b>Version</b> 1.0
   </div>
-  <!-- Main Footer -->
-  <footer class="main-footer">
-    <strong>
-      Copyright &copy; 2020
-
-      <a title="School SysManager" rel="external" type="text/html" hreflang="en" href="https://technostudy.co.in">School Management System</a>.
-    </strong>
-
-    All rights reserved.
-
-    <div class="float-right d-none d-sm-inline-block">
-      <b>Version</b> 1.0
-    </div>
-  </footer>
-</div><!-- ./wrapper -->
-
-<!--
-  1. jQuery
-  2. Bootstrap
-  3. overlayScrollbars
-  4. AdminLTE App
-  5. OPTIONAL SCRIPTS
--->
-<script src="../plugins/jquery/jquery.min.js"></script>
-<script src="../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-<script src="../plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
-<script src="../assets/js/adminlte.js"></script>
-<script src="../assets/js/demo.js"></script>
-<script src="./index.js"></script>
-</body>
-</html>
+</footer>
