f76f0b4050939b86cccae5c40b2e36e901df3b55
Cassiano Castro <development.castro@gmail.com>
2023-12-30 18:56:28 -0300

    Moved content to ./.components/aside/index.js

--- teacher/index.js
+++ teacher/index.js
@@ -5,27 +5,7 @@
  */
 function index()
 {
-    var path = window.location.href;
-    // console.log(path);
-
-    $(".nav-link").each(() => {
-        var href = $(this).attr('href');
-
-        // console.log(href);
-
-        if ( path === decodeURIComponent(href) )
-        {
-            $(this).addClass('active');
-
-            var parent = $(this).closest('.has-treeview');
-
-            parent.addClass('menu-open');
-
-            $(parent).find('.nav-link').first().addClass('active');
-
-            // console.log(parent);
-        }
-    });
+    //
 }
 
 index();
