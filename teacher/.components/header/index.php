5b1a503fc9c1d4c472daf0b588e1ff9e22d12ca7
Cassiano Castro <development.castro@gmail.com>
2023-12-30 19:01:25 -0300

    HTML restructured, removed class attribute from several elements

--- teacher/.components/header/index.php
+++ teacher/.components/header/index.php
@@ -1,190 +1,195 @@
-<header class="main-header navbar navbar-expand navbar-white navbar-light">
-  <!-- Left navbar links -->
-  <ul class="navbar-nav">
-    <li class="nav-item">
-      <a class="nav-link" data-widget="pushmenu" role="button" href="#">
-        <i class="fas fa-bars"></i>
-      </a>
-    </li>
+<header>
+  <div>
 
-    <li class="nav-item d-none d-sm-inline-block">
-      <a class="nav-link" href="#">
-        Home
-      </a>
-    </li>
+    <nav>
+      <div>
+        <ul>
+          <li class="nav-item">
+            <a class="nav-link" data-widget="pushmenu" role="button" href="#">
+              <span class="fas fa-bars"></span>
+            </a>
+          </li>
 
-    <li class="nav-item d-none d-sm-inline-block">
-      <a class="nav-link" href="#">
-        Contact
-      </a>
-    </li>
-  </ul>
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              Home
+            </a>
+          </li>
 
-  <!-- SEARCH FORM -->
-  <form class="form-inline ml-3">
-    <div class="input-group input-group-sm">
+          <li class="nav-item">
+            <a class="nav-link" href="#">
+              Contact
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
 
-      <input type="search" placeholder="Search" aria-label="Search" class="form-control form-control-navbar">
+    <form action="./" method="post" autocomplete="off">
+      <div>
+        <label>
+          <span>Search</span>
 
-      <div class="input-group-append">
-        <button type="submit" class="btn btn-navbar">
-          <i class="fas fa-search"></i>
+          <input type="search" placeholder="Search" aria-label="Search">
+        </label>
+
+        <button type="submit" title="Search">
+          <span class="fas fa-search"></span>
         </button>
       </div>
+    </form>
 
-    </div>
-  </form>
+    <menu>
+      <!-- Messages Dropdown Menu -->
+      <li class="dropdown">
+        <button type="button" title="Comments" data-toggle="dropdown">
+          <span class="far fa-comments"></span>
 
-  <!-- Right navbar links -->
-  <ul class="navbar-nav ml-auto">
-    <!-- Messages Dropdown Menu -->
-    <li class="nav-item dropdown">
-      <a class="nav-link" data-toggle="dropdown" href="#">
-        <i class="far fa-comments"></i>
+          <span>3</span>
+        </button>
 
-        <span class="badge badge-danger navbar-badge">3</span>
-      </a>
+        <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+          <a class="dropdown-item" href="#">
+            <!-- Message Start -->
+            <div class="media">
+              <img src="<?= $site_url ?>/assets/img/user1-128x128.jpg" alt="User Avatar" class="img-size-50 mr-3 img-circle">
 
-      <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-        <a class="dropdown-item" href="#">
-          <!-- Message Start -->
-          <div class="media">
-            <img src="<?= $site_url ?>/assets/img/user1-128x128.jpg" alt="User Avatar" class="img-size-50 mr-3 img-circle">
+              <div class="media-body">
+                <h3 class="dropdown-item-title">
+                  Brad Diesel
 
-            <div class="media-body">
-              <h3 class="dropdown-item-title">
-                Brad Diesel
+                  <span class="float-right text-sm text-danger">
+                    <i class="fas fa-star"></i>
+                  </span>
+                </h3>
 
-                <span class="float-right text-sm text-danger">
-                  <i class="fas fa-star"></i>
-                </span>
-              </h3>
+                <p class="text-sm">
+                  Call me whenever you can...
+                </p>
 
-              <p class="text-sm">
-                Call me whenever you can...
-              </p>
-
-              <p class="text-sm text-muted">
-                <i class="far fa-clock mr-1"></i> 4 Hours Ago
-              </p>
+                <p class="text-sm text-muted">
+                  <i class="far fa-clock mr-1"></i> 4 Hours Ago
+                </p>
+              </div>
             </div>
-          </div>
-          <!-- Message End -->
-        </a>
+            <!-- Message End -->
+          </a>
 
-        <div class="dropdown-divider"></div>
+          <div class="dropdown-divider"></div>
 
-        <a class="dropdown-item" href="#">
-          <!-- Message Start -->
-          <div class="media">
-            <img src="<?= $site_url ?>/assets/img/user8-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
+          <a class="dropdown-item" href="#">
+            <!-- Message Start -->
+            <div class="media">
+              <img src="<?= $site_url ?>/assets/img/user8-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
 
-            <div class="media-body">
-              <h3 class="dropdown-item-title">
-                John Pierce
+              <div class="media-body">
+                <h3 class="dropdown-item-title">
+                  John Pierce
 
-                <span class="float-right text-sm text-muted">
-                  <i class="fas fa-star"></i>
-                </span>
-              </h3>
+                  <span class="float-right text-sm text-muted">
+                    <i class="fas fa-star"></i>
+                  </span>
+                </h3>
 
-              <p class="text-sm">
-                I got your message bro
-              </p>
+                <p class="text-sm">
+                  I got your message bro
+                </p>
 
-              <p class="text-sm text-muted">
-                <i class="far fa-clock mr-1"></i> 4 Hours Ago
-              </p>
+                <p class="text-sm text-muted">
+                  <i class="far fa-clock mr-1"></i> 4 Hours Ago
+                </p>
+              </div>
             </div>
-          </div>
-          <!-- Message End -->
-        </a>
+            <!-- Message End -->
+          </a>
 
-        <div class="dropdown-divider"></div>
+          <div class="dropdown-divider"></div>
 
-        <a class="dropdown-item" href="#">
-          <!-- Message Start -->
-          <div class="media">
-            <img src="<?= $site_url ?>/assets/img/user3-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
+          <a class="dropdown-item" href="#">
+            <!-- Message Start -->
+            <div class="media">
+              <img src="<?= $site_url ?>/assets/img/user3-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
 
-            <div class="media-body">
-              <h3 class="dropdown-item-title">
-                Nora Silvester
+              <div class="media-body">
+                <h3 class="dropdown-item-title">
+                  Nora Silvester
 
-                <span class="float-right text-sm text-warning">
-                  <i class="fas fa-star"></i>
-                </span>
-              </h3>
+                  <span class="float-right text-sm text-warning">
+                    <i class="fas fa-star"></i>
+                  </span>
+                </h3>
 
-              <p class="text-sm">
-                The subject goes here
-              </p>
+                <p class="text-sm">
+                  The subject goes here
+                </p>
 
-              <p class="text-sm text-muted">
-                <i class="far fa-clock mr-1"></i> 4 Hours Ago
-              </p>
+                <p class="text-sm text-muted">
+                  <i class="far fa-clock mr-1"></i> 4 Hours Ago
+                </p>
+              </div>
             </div>
-          </div>
-          <!-- Message End -->
-        </a>
+            <!-- Message End -->
+          </a>
 
-        <div class="dropdown-divider"></div>
+          <div class="dropdown-divider"></div>
 
-        <a class="dropdown-item dropdown-footer" href="#">
-          See All Messages
-        </a>
-      </div>
-    </li>
+          <a class="dropdown-item dropdown-footer" href="#">
+            See All Messages
+          </a>
+        </div>
+      </li>
 
-    <!-- Notifications Dropdown Menu -->
-    <li class="nav-item dropdown">
-      <a class="nav-link" data-toggle="dropdown" href="#">
-        <i class="far fa-bell"></i>
+      <!-- Notifications Dropdown Menu -->
+      <li class="dropdown">
+        <button type="button" title="Notifications" data-toggle="dropdown">
+          <span class="far fa-bell"></span>
 
-        <span class="badge badge-warning navbar-badge">15</span>
-      </a>
+          <span>15</span>
+        </button>
 
-      <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-        <span class="dropdown-item dropdown-header">15 Notifications</span>
+        <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+          <span class="dropdown-item dropdown-header">15 Notifications</span>
 
-        <div class="dropdown-divider"></div>
+          <div class="dropdown-divider"></div>
 
-        <a href="#" class="dropdown-item">
-          <i class="fas fa-envelope mr-2"></i> 4 new messages
+          <a href="#" class="dropdown-item">
+            <i class="fas fa-envelope mr-2"></i> 4 new messages
 
-          <span class="float-right text-muted text-sm">3 mins</span>
-        </a>
+            <span class="float-right text-muted text-sm">3 mins</span>
+          </a>
 
-        <div class="dropdown-divider"></div>
+          <div class="dropdown-divider"></div>
 
-        <a href="#" class="dropdown-item">
-          <i class="fas fa-users mr-2"></i> 8 friend requests
+          <a href="#" class="dropdown-item">
+            <i class="fas fa-users mr-2"></i> 8 friend requests
 
-          <span class="float-right text-muted text-sm">12 hours</span>
-        </a>
+            <span class="float-right text-muted text-sm">12 hours</span>
+          </a>
 
-        <div class="dropdown-divider"></div>
+          <div class="dropdown-divider"></div>
 
-        <a href="#" class="dropdown-item">
-          <i class="fas fa-file mr-2"></i> 3 new reports
+          <a href="#" class="dropdown-item">
+            <i class="fas fa-file mr-2"></i> 3 new reports
 
-          <span class="float-right text-muted text-sm">2 days</span>
-        </a>
+            <span class="float-right text-muted text-sm">2 days</span>
+          </a>
 
-        <div class="dropdown-divider"></div>
+          <div class="dropdown-divider"></div>
 
-        <a href="#" class="dropdown-item dropdown-footer">
-          See All Notifications
-        </a>
-      </div>
-    </li>
+          <a href="#" class="dropdown-item dropdown-footer">
+            See All Notifications
+          </a>
+        </div>
+      </li>
 
-    <li class="nav-item">
-      <a title="Logout" class="nav-link" href="<?= $site_url ?>/actions/logout.php">
-        Logout
+      <li>
+        <button type="button" title="Logout">
+          <span class="fa fa-sign-out-alt"></span>
 
-        <i class="fa fa-sign-out-alt"></i>
-      </a>
-    </li>
-  </ul>
+          <span>Logout</span>
+        </button>
+      </li>
+    </menu>
+
+  </div>
 </header>
