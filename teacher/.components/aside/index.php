43dfadaa5fa6d88f9a8b2ada6ab4131bba4b8b02
Cassiano Castro <development.castro@gmail.com>
2023-12-30 18:53:09 -0300

    HTML restructured, commented empty links, removed class attribute from several elements

--- teacher/.components/aside/index.php
+++ teacher/.components/aside/index.php
@@ -1,202 +1,203 @@
-<aside class="main-sidebar sidebar-dark-primary elevation-4">
+<aside>
+  <div>
 
-  <a class="brand-link" href="<?= $site_url ?>">
-    <img
-      src="<?= $site_url ?>/assets/img/AdminLTELogo.png"
-      alt="AdminLTE Logo"
-      style="opacity: .8"
-      class="brand-image img-circle elevation-3"
-    >
+    <nav>
+      <div>
+        <ul data-widget="treeview" data-accordion="false" role="menu">
+          <li>
+            <a href="<?= $site_url ?>">
+              <figure>
+                <img
+                  src="<?= $site_url ?>/assets/img/AdminLTELogo.png"
+                  alt="AdminLTE Logo"
+                  height="50"
+                  width="50"
+                >
 
-    <span class="brand-text font-weight-light">Teacher Panel</span>
-  </a>
+                <figcaption>Teacher Panel</figcaption>
+              </figure>
+            </a>
+          </li>
 
-  <div class="sidebar">
+          <!--
+            Add icons to the links using the .nav-icon class
+            with font-awesome or any other icon font library
+          -->
+          <li class="nav-item">
+            <a class="nav-link" href="<?= $site_url ?>/teacher">
+              <span class="fas fa-tachometer-alt"></span>
 
-    <nav class="mt-2">
-      <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-        <!--
-          Add icons to the links using the .nav-icon class
-          with font-awesome or any other icon font library
-        -->
-        <li class="nav-item">
-          <a class="nav-link" href="<?= $site_url ?>/teacher">
-            <span class="nav-icon fas fa-tachometer-alt"></span>
+              <span>Dashboard</span>
+            </a>
+          </li>
 
-            <span>Dashboard</span>
-          </a>
-        </li>
+          <!-- <li class="nav-item has-treeview">
+            <a class="nav-link" href="#">
+              <span class="nav-icon fas fa-users"></span>
 
-        <!-- <li class="nav-item has-treeview">
-          <a class="nav-link" href="#">
-            <span class="nav-icon fas fa-users"></span>
+              <p>
+                Manage Classes
 
-            <p>
-              Manage Classes
+                <span class="fas fa-angle-left right"></span>
+              </p>
+            </a>
 
-              <span class="fas fa-angle-left right"></span>
-            </p>
-          </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a class="nav-link" href="<?= $site_url ?>/teacher/courses.php">
+                  <span class="far fa-circle nav-icon"></span>
 
-          <ul class="nav nav-treeview">
-            <li class="nav-item">
-              <a class="nav-link" href="<?= $site_url ?>/teacher/courses.php">
-                <span class="far fa-circle nav-icon"></span>
+                  <span>Courses</span>
+                </a>
+              </li>
 
-                <span>Courses</span>
-              </a>
-            </li>
+              <li class="nav-item">
+                <a class="nav-link" href="<?=$site_url?>/teacher/lessons.php">
+                  <span class="far fa-circle nav-icon"></span>
 
-            <li class="nav-item">
-              <a class="nav-link" href="<?=$site_url?>/teacher/lessons.php">
-                <span class="far fa-circle nav-icon"></span>
+                  <span>Lessons</span>
+                </a>
+              </li>
 
-                <span>Lessons</span>
-              </a>
-            </li>
+              <li class="nav-item">
+                <a class="nav-link" href="<?=$site_url?>/teacher/subjects.php">
+                  <span class="far fa-circle nav-icon"></span>
 
-            <li class="nav-item">
-              <a class="nav-link" href="<?=$site_url?>/teacher/subjects.php">
-                <span class="far fa-circle nav-icon"></span>
+                  <span>Subjects</span>
+                </a>
+              </li>
+            </ul>
+          </li> -->
 
-                <span>Subjects</span>
-              </a>
-            </li>
-          </ul>
-        </li> -->
+          <li class="nav-item has-treeview">
+            <a class="nav-link" href="#">
+              <span class="fas fa-users"></span>
 
-        <li class="nav-item has-treeview">
-          <a class="nav-link" href="#">
-            <span class="nav-icon fas fa-users"></span>
-
-            <p>
-              Manage Class Routines
-
-              <i class="fas fa-angle-left right"></i>
-            </p>
-          </a>
-
-          <ul class="nav nav-treeview">
-            <li class="nav-item">
-              <a class="nav-link" href="<?= $site_url ?>/teacher/.components/routines/.components/periods">
-                <span class="far fa-circle nav-icon"></span>
-
-                <span>Periods</span>
-              </a>
-            </li>
-
-            <li class="nav-item">
-              <a class="nav-link" href="<?= $site_url ?>/teacher/.components/routines/.components/timetable">
-                <span class="far fa-circle nav-icon"></span>
-
-                <span>Time Table</span>
-              </a>
-            </li>
-          </ul>
-        </li>
-
-        <!-- <li class="nav-item has-treeview">
-          <a class="nav-link" href="#">
-            <span class="nav-icon fas fa-users"></span>
-
-            <p>
-              Manage Examinations
+              <span>Manage Class Routines</span>
 
               <span class="fas fa-angle-left right"></span>
-            </p>
-          </a>
-        </li>
+            </a>
 
-        <li class="nav-item has-treeview">
-          <a class="nav-link" href="#">
-            <span class="nav-icon fas fa-users"></span>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a class="nav-link" href="<?= $site_url ?>/teacher/.components/routines/.components/periods">
+                  <span class="far fa-circle"></span>
 
-            <p>
-              Manage Attendance
+                  <span>Periods</span>
+                </a>
+              </li>
 
-              <span class="fas fa-angle-left right"></span>
-            </p>
-          </a>
+              <li class="nav-item">
+                <a class="nav-link" href="<?= $site_url ?>/teacher/.components/routines/.components/timetable">
+                  <span class="far fa-circle"></span>
 
-          <ul class="nav nav-treeview">
-            <li class="nav-item">
-              <a class="nav-link" href="<?= $site_url ?>/teacher/attendance.php">
-                <span class="far fa-circle nav-icon"></span>
+                  <span>Time Table</span>
+                </a>
+              </li>
+            </ul>
+          </li>
 
-                <span>Attendance</span>
-              </a>
-            </li>
-          </ul>
-        </li>
+          <!-- <li class="nav-item has-treeview">
+            <a class="nav-link" href="#">
+              <span class="nav-icon fas fa-users"></span>
 
-        <li class="nav-item has-treeview">
-          <a class="nav-link" href="#">
-            <span class="nav-icon fas fa-users"></span>
+              <p>
+                Manage Examinations
 
-            <p>
-              Fee Details
+                <span class="fas fa-angle-left right"></span>
+              </p>
+            </a>
+          </li>
 
-              <span class="fas fa-angle-left right"></span>
-            </p>
-          </a>
+          <li class="nav-item has-treeview">
+            <a class="nav-link" href="#">
+              <span class="nav-icon fas fa-users"></span>
 
-          <ul class="nav nav-treeview">
-            <li class="nav-item">
-              <a class="nav-link" href="<?= $site_url ?>/teacher/fee-details.php">
-                <span class="far fa-circle nav-icon"></span>
+              <p>
+                Manage Attendance
 
-                <span>Fee Details</span>
-              </a>
-            </li>
-          </ul>
-        </li> -->
+                <span class="fas fa-angle-left right"></span>
+              </p>
+            </a>
 
-        <li class="nav-item has-treeview">
-          <a class="nav-link" href="#">
-            <span class="nav-icon fas fa-users"></span>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a class="nav-link" href="<?= $site_url ?>/teacher/attendance.php">
+                  <span class="far fa-circle nav-icon"></span>
 
-            <p>
-              Study Materials
+                  <span>Attendance</span>
+                </a>
+              </li>
+            </ul>
+          </li>
 
-              <span class="fas fa-angle-left right"></span>
-            </p>
-          </a>
+          <li class="nav-item has-treeview">
+            <a class="nav-link" href="#">
+              <span class="nav-icon fas fa-users"></span>
 
-          <ul class="nav nav-treeview">
-            <li class="nav-item">
-              <a class="nav-link" href="<?= $site_url ?>/teacher/.components/study-materials">
-                <span class="far fa-circle nav-icon"></span>
+              <p>
+                Fee Details
 
-                <span>Study Materials</span>
-              </a>
-            </li>
-          </ul>
-        </li>
+                <span class="fas fa-angle-left right"></span>
+              </p>
+            </a>
 
-        <!-- <li class="nav-item has-treeview">
-          <a class="nav-link" href="#">
-            <span class="nav-icon fas fa-users"></span>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a class="nav-link" href="<?= $site_url ?>/teacher/fee-details.php">
+                  <span class="far fa-circle nav-icon"></span>
 
-            <p>
-              Manage Events
+                  <span>Fee Details</span>
+                </a>
+              </li>
+            </ul>
+          </li> -->
 
-              <span class="fas fa-angle-left right"></span>
-            </p>
-          </a>
-        </li>
+          <li class="nav-item has-treeview">
+            <a class="nav-link" href="#">
+              <span class="fas fa-users"></span>
 
-        <li class="nav-item has-treeview">
-          <a class="nav-link" href="#">
-            <span class="nav-icon fas fa-users"></span>
-
-            <p>
-              Communications
+              <span>Study Materials</span>
 
               <span class="fas fa-angle-left right"></span>
-            </p>
-          </a>
-        </li> -->
-      </ul>
+            </a>
+
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a class="nav-link" href="<?= $site_url ?>/teacher/.components/study-materials">
+                  <span class="far fa-circle"></span>
+
+                  <span>Study Materials</span>
+                </a>
+              </li>
+            </ul>
+          </li>
+
+          <!-- <li class="nav-item has-treeview">
+            <a class="nav-link" href="#">
+              <span class="nav-icon fas fa-users"></span>
+
+              <p>
+                Manage Events
+
+                <span class="fas fa-angle-left right"></span>
+              </p>
+            </a>
+          </li>
+
+          <li class="nav-item has-treeview">
+            <a class="nav-link" href="#">
+              <span class="nav-icon fas fa-users"></span>
+
+              <p>
+                Communications
+
+                <span class="fas fa-angle-left right"></span>
+              </p>
+            </a>
+          </li> -->
+        </ul>
+      </div>
     </nav>
 
   </div>
