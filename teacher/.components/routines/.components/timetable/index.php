24f8287f0f7485b1214da9118f16c1ae44a0c837
Cassiano Castro <development.castro@gmail.com>
2023-12-31 14:27:22 -0300

    Restructured HTML

--- teacher/.components/routines/.components/timetable/index.php
+++ teacher/.components/routines/.components/timetable/index.php
@@ -13,153 +13,153 @@ require_once __DIR__ . '/../../../../../actions/teacher.php';
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
 
-  <link rel="stylesheet" type="text/css" href="../../../../../plugins/fontawesome-free/css/all.min.css">
-  <link rel="stylesheet" type="text/css" href="../../../../../plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
-  <link rel="stylesheet" type="text/css" href="../../../../../assets/css/adminlte.min.css">
   <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700">
+  <link rel="stylesheet" type="text/css" href="/plugins/fontawesome-free/css/all.min.css">
+  <link rel="stylesheet" type="text/css" href="/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+  <link rel="stylesheet" type="text/css" href="/assets/css/adminlte.min.css">
   <link rel="stylesheet" type="text/css" href="./index.css">
 
   <title>Teacher's Dashboard | School SysManager</title>
 </head>
-<body class="hold-transition sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed">
-  <div class="wrapper">
+<body>
+  <?php require_once __DIR__ . '/../../../header/index.php'; ?>
 
-    <?php require_once __DIR__ . '/../../../header/index.php'; ?>
+  <main>
+    <div>
 
-    <main class="content-wrapper">
-      <div>
+      <section id="timetable">
+        <div>
 
-        <section class="content">
-          <div class="container-fluid">
+          <header>
+            <div>
+              <h1>Time Table</h1>
 
-            <header>
-              <div>
-                <h1>Time Table</h1>
+              <nav>
+                <div>
+                  <ul>
+                    <li><a href="#">Teacher</a></li>
+                    <li>/</li>
+                    <li><a href="#">Time Table</a></li>
+                  </ul>
+                </div>
+              </nav>
+            </div>
+          </header>
 
-                <nav>
-                  <div>
-                    <ul>
-                      <li><a href="#">Teacher</a></li>
-                      <li>/</li>
-                      <li><a href="#">Time Table</a></li>
-                    </ul>
-                  </div>
-                </nav>
-              </div>
-            </header>
+          <table class="table">
+            <thead>
+              <tr>
+                <th>Timing</th>
+                <th>Monday</th>
+                <th>Tuesday</th>
+                <th>Wednesday</th>
+                <th>Thursday</th>
+                <th>Friday</th>
+                <th>Saturday</th>
+                <th>Sunday</th>
+              </tr>
+            </thead>
+            <tbody>
+              <?php
+              $args    = ['type' => 'period', 'status' => 'publish'];
+              $periods = get_posts($args);
 
-            <table class="table">
-              <thead>
-                <tr>
-                  <th>Timing</th>
-                  <th>Monday</th>
-                  <th>Tuesday</th>
-                  <th>Wednesday</th>
-                  <th>Thursday</th>
-                  <th>Friday</th>
-                  <th>Saturday</th>
-                  <th>Sunday</th>
-                </tr>
-              </thead>
-              <tbody>
+              foreach ( $periods as $period )
+              {
+                $from = get_metadata($period->id, 'from')[0]->meta_value;
+                $to   = get_metadata($period->id, 'to')[0]->meta_value;
+              ?>
+              <tr>
+                <td>
+                  <?= date('h:i A', strtotime($from)) ?> -
+                  <?= date('h:i A', strtotime($to)) ?>
+                </td>
                 <?php
-                $args    = ['type' => 'period', 'status' => 'publish'];
-                $periods = get_posts($args);
+                  $days = ['monday','tuesday','wednesday','thursday','friday','saturday','sunday'];
 
-                foreach ( $periods as $period )
-                {
-                  $from = get_metadata($period->id, 'from')[0]->meta_value;
-                  $to   = get_metadata($period->id, 'to')[0]->meta_value;
-                ?>
-                <tr>
-                  <td><?= date('h:i A', strtotime($from)) ?> - <?= date('h:i A', strtotime($to)) ?></td>
-                  <?php
-                    $days = ['monday','tuesday','wednesday','thursday','friday','saturday','sunday'];
+                  foreach ( $days as $day )
+                  {
+                    $query = mysqli_query(
+                      $db_conn,
+                      <<<SQL
+                        SELECT * FROM
+                          posts as p
+                        INNER JOIN
+                          metadata as mc ON (mc.item_id = p.id)
+                        INNER JOIN
+                          metadata as md ON (md.item_id = p.id)
+                        INNER JOIN
+                          metadata as mp ON (mp.item_id = p.id)
+                        WHERE
+                          p.type = 'timetable'
+                        AND
+                          p.status = 'publish'
+                        AND
+                          md.meta_key = 'day_name'
+                        AND
+                          md.meta_value = '$day'
+                        AND
+                          mp.meta_key = 'period_id'
+                        AND
+                          mp.meta_value = $period->id
+                        AND
+                          mc.meta_key = 'class_id'
+                        AND
+                          mc.meta_value = 1
+                      SQL
+                    );
 
-                    foreach ( $days as $day )
+                    if ( mysqli_num_rows($query) > 0 )
                     {
-                      $query = mysqli_query(
-                        $db_conn,
-                        <<<SQL
-                          SELECT * FROM
-                            posts as p
-                          INNER JOIN
-                            metadata as mc ON (mc.item_id = p.id)
-                          INNER JOIN
-                            metadata as md ON (md.item_id = p.id)
-                          INNER JOIN
-                            metadata as mp ON (mp.item_id = p.id)
-                          WHERE
-                            p.type = 'timetable'
-                          AND
-                            p.status = 'publish'
-                          AND
-                            md.meta_key = 'day_name'
-                          AND
-                            md.meta_value = '$day'
-                          AND
-                            mp.meta_key = 'period_id'
-                          AND
-                            mp.meta_value = $period->id
-                          AND
-                            mc.meta_key = 'class_id'
-                          AND
-                            mc.meta_value = 1
-                        SQL
-                      );
-
-                      if ( mysqli_num_rows($query) > 0 )
+                      while ( $timetable = mysqli_fetch_object($query) )
                       {
-                        while ( $timetable = mysqli_fetch_object($query) )
-                        {
-                  ?>
-                          <td>
-                            <p>
-                              <b>Teacher: </b>
-                              <?php
-                              $teacher_id = get_metadata($timetable->item_id, 'teacher_id')[0]->meta_value;
+                ?>
+                        <td>
+                          <p>
+                            <b>Teacher: </b>
+                            <?php
+                            $teacher_id = get_metadata($timetable->item_id, 'teacher_id')[0]->meta_value;
 
-                              // echo get_user_data($teacher_id)[0]->name;
-                              echo get_user_data($teacher_id)->name;
-                              ?>
+                            // echo get_user_data($teacher_id)[0]->name;
+                            echo get_user_data($teacher_id)->name;
+                            ?>
 
-                              <br>
-                              <b>Subject: </b>
-                              <?php
-                              $subject_id = get_metadata($timetable->item_id, 'subject_id')[0]->meta_value;
+                            <br>
+                            <b>Subject: </b>
+                            <?php
+                            $subject_id = get_metadata($timetable->item_id, 'subject_id')[0]->meta_value;
 
-                              echo get_post(['id' => $subject_id])->title;
-                              ?>
-                            </p>
-                          </td>
-                      <?php }
-                      } else { ?>
-                          <td>
-                            Unscheduled
-                          </td>
-                      <?php }
-                    }?>
-                </tr>
-                <?php } ?>
-              </tbody>
-            </table>
+                            echo get_post(['id' => $subject_id])->title;
+                            ?>
+                          </p>
+                        </td>
+                    <?php }
+                    } else { ?>
+                        <td>
+                          Unscheduled
+                        </td>
+                    <?php }
+                  }?>
+              </tr>
+              <?php } ?>
+            </tbody>
+          </table>
 
-          </div>
-        </section>
+        </div>
+      </section>
 
-      </div>
-    </main>
+    </div>
+  </main>
 
-    <?php require_once __DIR__ . '/../../../footer/index.php'; ?>
+  <?php require_once __DIR__ . '/../../../footer/index.php'; ?>
 
-    <?php require_once __DIR__ . '/../../../aside/index.php'; ?>
-  </div>
+  <?php require_once __DIR__ . '/../../../aside/index.php'; ?>
 
-  <script defer src="../../../../../plugins/jquery/jquery.min.js"></script>
-  <script defer src="../../../../../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script defer src="../../../../../plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
-  <script defer src="../../../../../assets/js/adminlte.js"></script>
-  <script defer src="../../../../../assets/js/demo.js"></script>
-  <script defer src="../../../../index.js"></script>
+  <script defer src="/plugins/jquery/jquery.min.js"></script>
+  <script defer src="/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script defer src="/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+  <script defer src="/assets/js/adminlte.js"></script>
+  <script defer src="/assets/js/demo.js"></script>
+  <!-- <script defer src="../../../../index.js"></script> -->
 </body>
 </html>
