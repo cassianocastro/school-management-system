803fdbf41af72dc270ee6e3556ab478b2f114111
Cassiano Castro <development.castro@gmail.com>
2023-12-31 16:18:12 -0300

    Added chooser menu for periods and time table components

--- teacher/.components/routines/index.php
+++ teacher/.components/routines/index.php
@@ -1,3 +1,21 @@
 <?php require_once __DIR__ . '/.components/periods/index.php'; ?>
 
 <?php require_once __DIR__ . '/.components/timetable/index.php'; ?>
+
+<menu>
+  <li>
+    <button type="button" title="Periods">
+      <span class="far fa-circle"></span>
+
+      <span>Periods</span>
+    </button>
+  </li>
+
+  <li>
+    <button type="button" title="Time Table">
+      <span class="far fa-circle"></span>
+
+      <span>Time Table</span>
+    </button>
+  </li>
+</menu>
