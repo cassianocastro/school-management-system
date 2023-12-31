dd62beb2e592d0cc4d44cc4fc593a53b0e63b4c1
Cassiano Castro <development.castro@gmail.com>
2023-12-31 13:59:16 -0300

    Import the header and sidebar modules

--- teacher/index.js
+++ teacher/index.js
@@ -1,11 +1,15 @@
 "use strict";
 
+import { index as Header } from "./.components/header/index.js";
+import { index as Aside } from "./.components/aside/index.js";
+
 /**
  *
  */
 function index()
 {
-    //
+    Header();
+    Aside();
 }
 
 index();
