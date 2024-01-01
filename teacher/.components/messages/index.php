594385f242c8475b1ba8f3a1083d137cf6b454ff
Cassiano Castro <development.castro@gmail.com>
2024-01-01 14:27:21 -0300

    Directory creation

---
+++ teacher/.components/messages/index.php
@@ -0,0 +1,89 @@
+<div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+  <a class="dropdown-item" href="#">
+    <!-- Message Start -->
+    <div class="media">
+      <img src="<?= SITE_URL ?>/assets/img/user1-128x128.jpg" alt="User Avatar" class="img-size-50 mr-3 img-circle">
+
+      <div class="media-body">
+        <h3 class="dropdown-item-title">
+          Brad Diesel
+
+          <span class="float-right text-sm text-danger">
+            <i class="fas fa-star"></i>
+          </span>
+        </h3>
+
+        <p class="text-sm">
+          Call me whenever you can...
+        </p>
+
+        <p class="text-sm text-muted">
+          <i class="far fa-clock mr-1"></i> 4 Hours Ago
+        </p>
+      </div>
+    </div>
+    <!-- Message End -->
+  </a>
+
+  <div class="dropdown-divider"></div>
+
+  <a class="dropdown-item" href="#">
+    <!-- Message Start -->
+    <div class="media">
+      <img src="<?= SITE_URL ?>/assets/img/user8-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
+
+      <div class="media-body">
+        <h3 class="dropdown-item-title">
+          John Pierce
+
+          <span class="float-right text-sm text-muted">
+            <i class="fas fa-star"></i>
+          </span>
+        </h3>
+
+        <p class="text-sm">
+          I got your message bro
+        </p>
+
+        <p class="text-sm text-muted">
+          <i class="far fa-clock mr-1"></i> 4 Hours Ago
+        </p>
+      </div>
+    </div>
+    <!-- Message End -->
+  </a>
+
+  <div class="dropdown-divider"></div>
+
+  <a class="dropdown-item" href="#">
+    <!-- Message Start -->
+    <div class="media">
+      <img src="<?= SITE_URL ?>/assets/img/user3-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
+
+      <div class="media-body">
+        <h3 class="dropdown-item-title">
+          Nora Silvester
+
+          <span class="float-right text-sm text-warning">
+            <i class="fas fa-star"></i>
+          </span>
+        </h3>
+
+        <p class="text-sm">
+          The subject goes here
+        </p>
+
+        <p class="text-sm text-muted">
+          <i class="far fa-clock mr-1"></i> 4 Hours Ago
+        </p>
+      </div>
+    </div>
+    <!-- Message End -->
+  </a>
+
+  <div class="dropdown-divider"></div>
+
+  <a class="dropdown-item dropdown-footer" href="#">
+    See All Messages
+  </a>
+</div>
