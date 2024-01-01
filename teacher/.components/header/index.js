3f56b60d616f2a3430a3211e76fd7691a134fa1d
Cassiano Castro <development.castro@gmail.com>
2024-01-01 16:57:44 -0300

    Added event listeners to menu buttons

--- teacher/.components/header/index.js
+++ teacher/.components/header/index.js
@@ -5,9 +5,53 @@
  */
 function index()
 {
-    const button = document.querySelector("button[title='Logout']");
+    const menu = document.querySelector("body > header menu");
 
-    button.addEventListener("click", () => location.assign('http://www.schoolsysmanager.com/actions/logout.php'));
+    {
+        const button = menu.querySelector("button[title='Sidebar']");
+
+        button.addEventListener("click", () => {});
+    }
+
+    {
+        const aside  = document.querySelector("#messages");
+
+        const button = menu.querySelector("button[title='Messages']");
+
+        button.addEventListener("click", () => {
+            if ( aside.style.display == "none" || aside.style.display == "" )
+            {
+                aside.style.display = "block";
+            }
+            else
+            {
+                aside.style.display = "none";
+            }
+        });
+    }
+
+    {
+        const aside  = document.querySelector("#notifications");
+
+        const button = menu.querySelector("button[title='Notifications']");
+
+        button.addEventListener("click", () => {
+            if ( aside.style.display == "none" || aside.style.display == "" )
+            {
+                aside.style.display = "block";
+            }
+            else
+            {
+                aside.style.display = "none";
+            }
+        });
+    }
+
+    {
+        const button = menu.querySelector("button[title='Logout']");
+
+        button.addEventListener("click", () => location.assign('http://www.schoolsysmanager.com/actions/logout.php'));
+    }
 }
 
 export { index };
