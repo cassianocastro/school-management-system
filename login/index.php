0ec8e4669ed4589f7e4abd2f1007a8858c6a597d
Cassiano Castro <development.castro@gmail.com>
2024-01-01 23:11:41 -0300

    Removed over-commenting

--- login/index.php
+++ login/index.php
@@ -16,12 +16,6 @@
   <link rel="preload" as="script" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.min.js">
   <link rel="preload" as="script" href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/js/mdb.min.js">
 
-  <!--
-    1. Font Awesome
-    2. Google Fonts
-    3. Bootstrap core CSS
-    4. Material Design Bootstrap
-  -->
   <link rel="stylesheet" type="text/css" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
   <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
   <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css">
@@ -67,12 +61,6 @@
     </div>
   </section>
 
-  <!--
-    1. JQuery
-    2. Bootstrap tooltips
-    3. Bootstrap core JavaScript
-    4. MDB core JavaScript
-  -->
   <script defer src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script defer src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.4/umd/popper.min.js"></script>
   <script defer src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.min.js"></script>
