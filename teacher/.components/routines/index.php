b691e265fb2a334562fc048bf616b903b1a039ed
Cassiano Castro <development.castro@gmail.com>
2024-01-01 22:52:25 -0300

    Surrounded menu in a section

--- teacher/.components/routines/index.php
+++ teacher/.components/routines/index.php
@@ -2,20 +2,30 @@
 
 <?php require_once __DIR__ . '/.components/timetable/index.php'; ?>
 
-<menu>
-  <li>
-    <button type="button" title="Periods">
-      <span class="far fa-circle"></span>
+<section id="routines">
+  <div>
+    <header>
+      <div>
+        <h2>Class Routines</h2>
+      </div>
+    </header>
 
-      <span>Periods</span>
-    </button>
-  </li>
+    <menu>
+      <li>
+        <button type="button" title="Periods">
+          <span class="far fa-circle"></span>
 
-  <li>
-    <button type="button" title="Time Table">
-      <span class="far fa-circle"></span>
+          <span>Periods</span>
+        </button>
+      </li>
 
-      <span>Time Table</span>
-    </button>
-  </li>
-</menu>
+      <li>
+        <button type="button" title="Time Table">
+          <span class="far fa-circle"></span>
+
+          <span>Time Table</span>
+        </button>
+      </li>
+    </menu>
+  </div>
+</section>
