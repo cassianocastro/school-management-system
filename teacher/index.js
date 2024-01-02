c5dd7a02b2e8a47b468dbc2a2c73b294244ed346
Cassiano Castro <development.castro@gmail.com>
2024-01-01 22:55:32 -0300

    Inclusion of ./.components/routines/index.js module

--- teacher/index.js
+++ teacher/index.js
@@ -3,15 +3,19 @@
 import { index as Header } from "./.components/header/index.js";
 import { index as Aside } from "./.components/aside/index.js";
 import { index as SM } from "./.components/study-materials/index.js";
+import { index as Routines } from "./.components/routines/index.js";
 
 /**
  *
  */
 function index()
 {
+    var zIndex = 0;
+
     Header();
-    Aside();
+    Aside(zIndex);
     SM();
+    Routines(zIndex);
 }
 
 index();
