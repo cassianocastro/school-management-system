30d384ad7165849489411bdf3a57cbabb9064bd6
Cassiano Castro <development.castro@gmail.com>
2024-01-04 16:28:15 -0300

    Moved JS code that shows the dialog on screen to study-materials/index.js

--- teacher/.components/study-materials/.components/table/index.js
+++ teacher/.components/study-materials/.components/table/index.js
@@ -5,10 +5,7 @@
  */
 function index()
 {
-    const dialog = document.querySelector("#smdialog");
-    const button = document.querySelector("button[title='Add new study material']");
-
-    button.addEventListener("click", () => dialog.showModal());
+    // code
 }
 
 export { index };
