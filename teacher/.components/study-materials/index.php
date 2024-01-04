25bbc0a009034f80f3370fa94766b109382b165f
Cassiano Castro <development.castro@gmail.com>
2024-01-04 16:23:46 -0300

    Added HTML structure to navigation and footer components and php script to generate the array of study materials

--- teacher/.components/study-materials/index.php
+++ teacher/.components/study-materials/index.php
@@ -86,6 +86,44 @@ if ( isset($_POST['submit']) )
 
 $classes  = get_posts(['type' => 'class', 'status' => 'publish']);
 $subjects = get_posts(['type' => 'subject', 'status' => 'publish']);
+
+
+$materials = [];
+$count     = 1;
+$query     = mysqli_query(
+  $db_conn,
+  <<<SQL
+    SELECT
+      *
+    FROM
+      posts
+    WHERE
+      type = 'study-material'
+    AND
+      author = 1
+  SQL
+);
+
+while ( $att = mysqli_fetch_object($query) )
+{
+  $class_id        = get_metadata($att->id, 'class')[0]->meta_value;
+  $class           = get_post(['id' => $class_id]);
+  $subject_id      = get_metadata($att->id, 'subject')[0]->meta_value;
+  $subject         = get_post(['id' => $subject_id]);
+  $file_attachment = get_metadata($att->id, 'file_attachment')[0]->meta_value;
+
+  // $file_attachment = get_post(['id' => $file_attachment]);
+  // echo '<pre>', print_r($class), '</pre>';
+
+  $materials[] = [
+    "count"         => $count++,
+    "att_title"     => $att->title,
+    "att_pubdate"   => $att->publish_date,
+    "class_title"   => $class->title,
+    "subject_title" => $subject->title,
+    "attachment"    => SITE_URL . "/assets/uploads/$file_attachment"
+  ];
+}
 ?>
 
 <section id="sm">
@@ -107,7 +145,27 @@ $subjects = get_posts(['type' => 'subject', 'status' => 'publish']);
       </div>
     </header>
 
-    <?php require_once __DIR__ . '/.components/table/index.php'; ?>
+    <nav>
+      <div>
+        <ul>
+          <?php foreach ( $materials as $material ) : ?>
+            <li>
+              <?php require __DIR__ . '/.components/table/index.php'; ?>
+            </li>
+          <?php endforeach; ?>
+        </ul>
+      </div>
+    </nav>
+
+    <footer>
+      <div>
+        <button type="button" title="Add new study material">
+          <span class="fa fa-plus"></span>
+
+          <span>Add New</span>
+        </button>
+      </div>
+    </footer>
 
     <?php require_once __DIR__ . '/.components/form/index.php'; ?>
   </div>
