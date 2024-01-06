c6ed3150b9e320fbaa7d28123d9989fb36843224
Cassiano Castro <development.castro@gmail.com>
2024-01-06 19:15:43 -0300

    Restructured HTML

--- student/.components/syllabus/courses.php
+++ student/.components/syllabus/courses.php
@@ -22,39 +22,39 @@
     <?php require_once __DIR__ . '/../header/index.php'; ?>
 
     <!-- Content Wrapper. Contains page content -->
-    <div class="content-wrapper">
+    <main class="content-wrapper">
+      <div>
 
-      <!-- Content Header (Page header) -->
-      <div class="content-header">
-        <div class="container-fluid">
-          <div class="row mb-2">
+        <!-- Main content -->
+        <section class="content">
+          <div class="container-fluid">
 
-            <div class="col-sm-6">
-              <h1 class="m-0 text-dark">Courses</h1>
+            <!-- Content Header (Page header) -->
+            <header class="content-header">
+              <div class="container-fluid">
+                <h1 class="m-0 text-dark">Courses</h1>
+
+                <nav class="row mb-2">
+                  <div class="col-sm-6">
+                    <ul class="breadcrumb float-sm-right">
+                      <li class="breadcrumb-item"><a href="#">Student</a></li>
+                      <li class="breadcrumb-item active"><a href="#">Courses</a></li>
+                    </ul>
+                  </div>
+                </nav>
+              </div>
+            </header>
+
+            <div class="card">
+              <div class="card-body">
+
+              </div>
             </div>
-
-            <div class="col-sm-6">
-              <ol class="breadcrumb float-sm-right">
-                <li class="breadcrumb-item"><a href="#">Student</a></li>
-                <li class="breadcrumb-item active">Courses</li>
-              </ol>
-            </div>
-
           </div>
-        </div>
+        </section>
+
       </div>
-
-      <!-- Main content -->
-      <section class="content">
-        <div class="container-fluid">
-          <div class="card">
-            <div class="card-body">
-
-            </div>
-          </div>
-        </div>
-      </section>
-    </div>
+    </main>
 
     <?php require_once __DIR__ . '/../footer/index.php'; ?>
 
