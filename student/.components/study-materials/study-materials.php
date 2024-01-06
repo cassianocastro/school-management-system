5e97ef016bd2bad412f1ed8ae60cc44c978f888f
Cassiano Castro <development.castro@gmail.com>
2024-01-06 19:25:38 -0300

    Restructured HTML header page

--- student/.components/study-materials/study-materials.php
+++ student/.components/study-materials/study-materials.php
@@ -25,24 +25,20 @@
     <div class="content-wrapper">
 
       <!-- Content Header (Page header) -->
-      <div class="content-header">
+      <header class="content-header">
         <div class="container-fluid">
-          <div class="row mb-2">
+          <h1 class="m-0 text-dark">Study Materials</h1>
 
+          <nav class="row mb-2">
             <div class="col-sm-6">
-              <h1 class="m-0 text-dark">Study Materials</h1>
-            </div>
-
-            <div class="col-sm-6">
-              <ol class="breadcrumb float-sm-right">
+              <ul class="breadcrumb float-sm-right">
                 <li class="breadcrumb-item"><a href="#">Student</a></li>
-                <li class="breadcrumb-item active">Study Materials</li>
-              </ol>
+                <li class="breadcrumb-item active"><a href="#">Study Materials</a></li>
+              </ul>
             </div>
-
-          </div>
+          </nav>
         </div>
-      </div>
+      </header>
 
       <!-- Main content -->
       <section class="content">
