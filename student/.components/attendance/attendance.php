fccb1759eeb6e745d35cc352e33ce3cb3550a919
Cassiano Castro <development.castro@gmail.com>
2024-01-08 18:55:59 -0300

    Updated URL of the linked resources

--- student/.components/attendance/attendance.php
+++ student/.components/attendance/attendance.php
@@ -14,10 +14,10 @@ $class    = get_post(['id' => $usermeta['class']]);
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
 
-  <link rel="stylesheet" type="text/css" href="../../../plugins/fontawesome-free/css/all.min.css">
-  <link rel="stylesheet" type="text/css" href="../../../plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
-  <link rel="stylesheet" type="text/css" href="../../../assets/css/adminlte.min.css">
   <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700">
+  <link rel="stylesheet" type="text/css" href="/plugins/fontawesome-free/css/all.min.css">
+  <link rel="stylesheet" type="text/css" href="/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+  <link rel="stylesheet" type="text/css" href="/assets/css/adminlte.min.css">
 
   <title>Student's Dashboard | School SysManager</title>
 </head>
@@ -108,11 +108,11 @@ $class    = get_post(['id' => $usermeta['class']]);
     <?php require_once __DIR__ . '/../aside/index.php'; ?>
   </div>
 
-  <script src="../../../plugins/jquery/jquery.min.js"></script>
-  <script src="../../../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="../../../plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
-  <script src="../../../assets/js/adminlte.js"></script>
-  <script src="../../../assets/js/demo.js"></script>
+  <script src="/plugins/jquery/jquery.min.js"></script>
+  <script src="/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+  <script src="/assets/js/adminlte.js"></script>
+  <script src="/assets/js/demo.js"></script>
   <script src="../../index.js"></script>
 </body>
 </html>
