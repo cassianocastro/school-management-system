6ef79876857ea97a8470014852e132ce3ed9f982
Cassiano Castro <development.castro@gmail.com>
2024-01-12 16:04:37 -0300

    Restructured HTML
    Removed HTML head & body elements and removed class attribute from section inner elements

--- student/.components/attendance/leave.php
+++ student/.components/attendance/leave.php
@@ -1,69 +1,24 @@
-<?php require_once __DIR__ . '/../../../includes/config.php'; ?>
+<section>
+  <div>
 
-<?php require_once __DIR__ . '/../../../actions/student.php'; ?>
-
-<!DOCTYPE html>
-<html lang="en-US" dir="ltr">
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="x-ua-compatible" content="ie=edge">
-
-  <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700">
-  <link rel="stylesheet" type="text/css" href="/plugins/fontawesome-free/css/all.min.css">
-  <link rel="stylesheet" type="text/css" href="/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
-  <link rel="stylesheet" type="text/css" href="/assets/css/adminlte.min.css">
-
-  <title>Student's Dashboard | School SysManager</title>
-</head>
-<body class="hold-transition sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed">
-  <div class="wrapper">
-
-    <?php require_once __DIR__ . '/../header/index.php'; ?>
-
-    <main class="content-wrapper">
+    <header>
       <div>
+        <h1>Leave</h1>
 
-        <section class="content">
-          <div class="container-fluid">
-
-            <header class="content-header">
-              <div class="container-fluid">
-                <h1 class="m-0 text-dark">Leave</h1>
-
-                <nav class="row mb-2">
-                  <div class="col-sm-6">
-                    <ul class="breadcrumb float-sm-right">
-                      <li class="breadcrumb-item"><a href="#">Student</a></li>
-                      <li class="breadcrumb-item active"><a href="#">Leave</a></li>
-                    </ul>
-                  </div>
-                </nav>
-              </div>
-            </header>
-
-            <div class="card">
-              <div class="card-body">
-
-              </div>
-            </div>
-
+        <nav>
+          <div>
+            <ul>
+              <li><a href="#">Student</a></li>
+              <li><a href="#">Leave</a></li>
+            </ul>
           </div>
-        </section>
-
+        </nav>
       </div>
-    </main>
+    </header>
 
-    <?php require_once __DIR__ . '/../footer/index.php'; ?>
+    <div>
 
-    <?php require_once __DIR__ . '/../aside/index.php'; ?>
+    </div>
+
   </div>
-
-  <script src="/plugins/jquery/jquery.min.js"></script>
-  <script src="/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
-  <script src="/assets/js/adminlte.js"></script>
-  <script src="/assets/js/demo.js"></script>
-  <script src="../../index.js"></script>
-</body>
-</html>
+</section>
