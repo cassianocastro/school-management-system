9c934b9056e331497ea85183fc9d41a92001685a
Cassiano Castro <development.castro@gmail.com>
2024-01-11 23:52:32 -0300

    Removed HTML head & body elements, such as class attribute from section inner elements

--- student/.components/routines/periods.php
+++ student/.components/routines/periods.php
@@ -20,85 +20,44 @@ foreach ( $periods as $period )
 }
 ?>
 
-<!DOCTYPE html>
-<html lang="en-US" dir="ltr">
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="x-ua-compatible" content="ie=edge">
+<section>
+  <div>
 
-  <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700">
-  <link rel="stylesheet" type="text/css" href="/plugins/fontawesome-free/css/all.min.css">
-  <link rel="stylesheet" type="text/css" href="/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
-  <link rel="stylesheet" type="text/css" href="/assets/css/adminlte.min.css">
-
-  <title>Student's Dashboard | School SysManager</title>
-</head>
-<body class="hold-transition sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed">
-  <div class="wrapper">
-
-    <?php require_once __DIR__ . '/../header/index.php'; ?>
-
-    <main class="content-wrapper">
+    <header>
       <div>
+        <h1>Periods</h1>
 
-        <section class="content">
-          <div class="container-fluid">
-
-            <header class="content-header">
-              <div class="container-fluid">
-                <h1 class="m-0 text-dark">Periods</h1>
-
-                <nav class="row mb-2">
-                  <div class="col-sm-6">
-                    <ul class="breadcrumb float-sm-right">
-                      <li class="breadcrumb-item"><a href="#">Student</a></li>
-                      <li class="breadcrumb-item active"><a href="#">Periods</a></li>
-                    </ul>
-                  </div>
-                </nav>
-              </div>
-            </header>
-
-            <div class="table-responsive">
-              <table class="table table-bordered">
-                <thead>
-                  <tr>
-                    <th>S.No.</th>
-                    <th>Title</th>
-                    <th>From</th>
-                    <th>To</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <?php foreach ( $rows as $row ) : ?>
-                    <tr>
-                      <td><?= $row["count"] ?></td>
-                      <td><?= $row["period"] ?></td>
-                      <td><?= $row["from"] ?></td>
-                      <td><?= $row["to"] ?></td>
-                    </tr>
-                  <?php endforeach; ?>
-                </toby>
-              </table>
-            </div>
-
+        <nav>
+          <div>
+            <ul>
+              <li><a href="#">Student</a></li>
+              <li><a href="#">Periods</a></li>
+            </ul>
           </div>
-        </section>
-
+        </nav>
       </div>
-    </main>
+    </header>
 
-    <?php require_once __DIR__ . '/../footer/index.php'; ?>
+    <table class="table">
+      <thead>
+        <tr>
+          <th>S.No.</th>
+          <th>Title</th>
+          <th>From</th>
+          <th>To</th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php foreach ( $rows as $row ) : ?>
+          <tr>
+            <td><?= $row["count"] ?></td>
+            <td><?= $row["period"] ?></td>
+            <td><?= $row["from"] ?></td>
+            <td><?= $row["to"] ?></td>
+          </tr>
+        <?php endforeach; ?>
+      </toby>
+    </table>
 
-    <?php require_once __DIR__ . '/../aside/index.php'; ?>
   </div>
-
-  <script src="/plugins/jquery/jquery.min.js"></script>
-  <script src="/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
-  <script src="/assets/js/adminlte.js"></script>
-  <script src="/assets/js/demo.js"></script>
-  <script src="../../index.js"></script>
-</body>
-</html>
+</section>
