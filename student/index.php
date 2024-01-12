c78946ceb0c64ac176832fcc9cd1233d68faab77
Cassiano Castro <development.castro@gmail.com>
2024-01-11 23:15:47 -0300

    Surrounded card components in a unordered list

--- student/index.php
+++ student/index.php
@@ -77,85 +77,90 @@ if ( isset($_POST['sign-out']) )
   <main>
     <div>
 
-      <div class="content-header">
-        <div class="container-fluid">
-          <div class="row mb-2">
-
-            <div class="col-sm-6">
-              <h1 class="m-0 text-dark">Dashboard</h1>
-            </div>
-
-            <div class="col-sm-6">
-              <ul class="breadcrumb float-sm-right">
-                <li class="breadcrumb-item"><a href="#">Student</a></li>
-                <li class="breadcrumb-item active">Dashboard</li>
-              </ul>
-            </div>
-
-          </div>
-        </div>
-      </div>
-
       <section class="content">
         <div class="container-fluid">
 
+          <header class="content-header">
+            <div class="container-fluid">
+              <h1 class="m-0 text-dark">Dashboard</h1>
+
+              <nav class="row mb-2">
+                <div class="col-sm-6">
+                  <ul class="breadcrumb float-sm-right">
+                    <li class="breadcrumb-item"><a href="#">Student</a></li>
+                    <li class="breadcrumb-item active"><a href="#">Dashboard</a></li>
+                  </ul>
+                </div>
+              </nav>
+            </div>
+          </header>
+
           <!-- Info boxes -->
-          <div class="row">
-            <div class="col-12 col-sm-6 col-md-3">
-              <div class="info-box">
-                <span class="info-box-icon bg-info elevation-1">
-                  <i class="fas fa-graduation-cap"></i>
-                </span>
+          <nav>
+            <div>
+              <ul>
+                <li>
+                  <div class="col-12 col-sm-6 col-md-3">
+                    <div class="info-box">
+                      <span class="info-box-icon bg-info elevation-1">
+                        <i class="fas fa-graduation-cap"></i>
+                      </span>
 
-                <div class="info-box-content">
-                  <span class="info-box-text">Total Students</span>
-                  <span class="info-box-number">2000</span>
-                </div>
-              </div>
+                      <div class="info-box-content">
+                        <span class="info-box-text">Total Students</span>
+                        <span class="info-box-number">2000</span>
+                      </div>
+                    </div>
+                  </div>
+                </li>
+
+                <li>
+                  <div class="col-12 col-sm-6 col-md-3">
+                    <div class="info-box mb-3">
+                      <span class="info-box-icon bg-danger elevation-1">
+                        <i class="fas fa-users"></i>
+                      </span>
+
+                      <div class="info-box-content">
+                        <span class="info-box-text">Total Teachers</span>
+                        <span class="info-box-number">50</span>
+                      </div>
+                    </div>
+                  </div>
+                </li>
+
+                <li>
+                  <div class="col-12 col-sm-6 col-md-3">
+                    <div class="info-box mb-3">
+                      <span class="info-box-icon bg-success elevation-1">
+                        <i class="fas fa-book-open"></i>
+                      </span>
+
+                      <div class="info-box-content">
+                        <span class="info-box-text">Total Courses</span>
+                        <span class="info-box-number">100</span>
+                      </div>
+                    </div>
+                  </div>
+                </li>
+
+                <li>
+                  <div class="col-12 col-sm-6 col-md-3">
+                    <div class="info-box mb-3">
+                      <span class="info-box-icon bg-warning elevation-1">
+                        <i class="fas fa-question"></i>
+                      </span>
+
+                      <div class="info-box-content">
+                        <span class="info-box-text">New Inquiries</span>
+                        <span class="info-box-number">10</span>
+                      </div>
+                    </div>
+                  </div>
+                </li>
+              </ul>
             </div>
-
-            <div class="col-12 col-sm-6 col-md-3">
-              <div class="info-box mb-3">
-                <span class="info-box-icon bg-danger elevation-1">
-                  <i class="fas fa-users"></i>
-                </span>
-
-                <div class="info-box-content">
-                  <span class="info-box-text">Total Teachers</span>
-                  <span class="info-box-number">50</span>
-                </div>
-              </div>
-            </div>
-
-            <!-- fix for small devices only -->
-            <div class="clearfix hidden-md-up"></div>
-
-            <div class="col-12 col-sm-6 col-md-3">
-              <div class="info-box mb-3">
-                <span class="info-box-icon bg-success elevation-1">
-                  <i class="fas fa-book-open"></i>
-                </span>
-
-                <div class="info-box-content">
-                  <span class="info-box-text">Total Courses</span>
-                  <span class="info-box-number">100</span>
-                </div>
-              </div>
-            </div>
-
-            <div class="col-12 col-sm-6 col-md-3">
-              <div class="info-box mb-3">
-                <span class="info-box-icon bg-warning elevation-1">
-                  <i class="fas fa-question"></i>
-                </span>
-
-                <div class="info-box-content">
-                  <span class="info-box-text">New Inquiries</span>
-                  <span class="info-box-number">10</span>
-                </div>
-              </div>
-            </div>
-          </div>
+          </nav>
 
           <hr>
 
