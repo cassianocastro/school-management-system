85575d3934db5a26175dfbf795a48004a4cf1c47
Cassiano Castro <development.castro@gmail.com>
2024-01-11 23:54:21 -0300

    Moved PHP script to students/index.php

--- student/.components/routines/periods.php
+++ student/.components/routines/periods.php
@@ -1,25 +1,3 @@
-<?php
-require_once __DIR__ . '/../../../includes/config.php';
-require_once __DIR__ . '/../../../actions/student.php';
-
-$rows    = [];
-$count   = 1;
-$periods = get_posts(['type' => 'period', 'status' => 'publish']);
-
-foreach ( $periods as $period )
-{
-  $from = get_metadata($period->id, 'from')[0]->meta_value;
-  $to   = get_metadata($period->id, 'to')[0]->meta_value;
-
-  $rows[] = [
-    "count"  => $count++,
-    "period" => $period->title,
-    "from"   => date('h:i A', strtotime($from)),
-    "to"     => date('h:i A', strtotime($to))
-  ];
-}
-?>
-
 <section>
   <div>
 
