b289d6602d1a334a9d6b811f44510ab7f8825bd1
Cassiano Castro <development.castro@gmail.com>
2024-01-11 23:44:16 -0300

    Removed class attributes from HTML elements

--- student/.components/syllabus/courses.php
+++ student/.components/syllabus/courses.php
@@ -1,22 +1,22 @@
-<section class="content">
-  <div class="container-fluid">
+<section>
+  <div>
 
-    <header class="content-header">
-      <div class="container-fluid">
-        <h1 class="m-0 text-dark">Courses</h1>
+    <header>
+      <div>
+        <h1>Courses</h1>
 
-        <nav class="row mb-2">
-          <div class="col-sm-6">
-            <ul class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Student</a></li>
-              <li class="breadcrumb-item active"><a href="#">Courses</a></li>
+        <nav>
+          <div>
+            <ul>
+              <li><a href="#">Student</a></li>
+              <li><a href="#">Courses</a></li>
             </ul>
           </div>
         </nav>
       </div>
     </header>
 
-    <div class="card">
+    <div>
 
     </div>
 
