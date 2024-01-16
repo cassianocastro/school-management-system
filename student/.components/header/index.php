51a76b57732033b3ea103e8f5f1e882c4dfccf39
Cassiano Castro <development.castro@gmail.com>
2024-01-16 17:39:35 -0300

    Removed "onclick" inline event listener from HTML menu buttons

--- student/.components/header/index.php
+++ student/.components/header/index.php
@@ -25,13 +25,13 @@
       </li>
 
       <li>
-        <button type="button" title="Profile" onclick="location.assign('<?= $site_url ?>/student/.components/profile')">
+        <button type="button" title="Profile">
           Profile
         </button>
       </li>
 
       <li>
-        <button type="button" title="Logout" onclick="location.assign('<?= $site_url ?>/actions/logout.php')">
+        <button type="button" title="Logout">
           <span class="fa fa-sign-out-alt"></span>
 
           <span>Logout</span>
