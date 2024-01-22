65529c87e8ff069b5ba84b3a77a7208bf094e2b0
Cassiano Castro <development.castro@gmail.com>
2024-01-22 16:01:39 -0300

    Restructured HTML of header component

--- student/.components/fee-details/.components/tuition/.components/table/index.php
+++ student/.components/fee-details/.components/tuition/.components/table/index.php
@@ -1,22 +1,18 @@
 <!-- Content Header (Page header) -->
-<div class="content-header">
+<header class="content-header">
   <div class="container-fluid">
-    <div class="row mb-2">
+    <h1 class="m-0 text-dark">Manage Student Fee Details</h1>
 
+    <nav class="row mb-2">
       <div class="col-sm-6">
-        <h1 class="m-0 text-dark">Manage Student Fee Details</h1>
-      </div>
-
-      <div class="col-sm-6">
-        <ol class="breadcrumb float-sm-right">
+        <ul class="breadcrumb float-sm-right">
           <li class="breadcrumb-item"><a href="#">Student</a></li>
-          <li class="breadcrumb-item active">Student Fee Details</li>
-        </ol>
+          <li class="breadcrumb-item active"><a href="#">Student Fee Details</a></li>
+        </ul>
       </div>
-
-    </div>
+    </nav>
   </div>
-</div>
+</header>
 
 <!-- Main content -->
 <section class="content">
