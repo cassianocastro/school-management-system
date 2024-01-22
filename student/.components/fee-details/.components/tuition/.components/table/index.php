db94073e19842ef04d1b1a6431df3164a649c2e2
Cassiano Castro <development.castro@gmail.com>
2024-01-22 16:17:29 -0300

    Added HTML caption element to table

--- student/.components/fee-details/.components/tuition/.components/table/index.php
+++ student/.components/fee-details/.components/tuition/.components/table/index.php
@@ -28,91 +28,80 @@ $class    = get_post(['id' => $usermeta['class']]);
       </div>
     </header>
 
-    <div class="card">
-      <div class="card-header">
-        <h3 class="card-title">Student Detail</h3>
-      </div>
+    <table class="table table-bordered">
+      <caption>
+        Tution Fee
+      </caption>
+      <thead>
+        <tr>
+          <th colspan="4">Student Detail</th>
+        </tr>
+        <tr>
+          <th colspan="3"><strong>Name: </strong><?= get_users(['id' => $std_id])[0]->name ?></th>
+          <th><strong>Class: </strong><?= $class->title ?></th>
+        </tr>
+        <tr>
+          <th>S.No</th>
+          <th>Month</th>
+          <th>Fee Status</th>
+          <th>Action</th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php
+        $sql = "SELECT m.meta_value as `month` FROM `posts` as p JOIN `metadata` as m ON p.id = m.item_id WHERE p.type = 'payment' AND p.author = $user_id AND m.meta_key = 'month' AND year(p.publish_date) = 2023";
 
-      <div class="card-body">
-        <strong>Name: </strong><?= get_users(['id' => $std_id])[0]->name ?>
+        $query = mysqli_query($db_conn, $sql);
+        $paid_fees = [];
 
-        <br>
+        while ( $row = mysqli_fetch_object($query) )
+        {
+          $paid_fees[] = strtolower($row->month);
+        }
 
-        <strong>Class: </strong><?= $class->title ?>
-      </div>
-    </div>
+        // echo '<pre>', print_r($paid_fees), '</pre>';
 
-    <div class="card">
-      <div class="card-header">
-        <h3 class="card-title">Tution Fee</h3>
-      </div>
+        $months = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december'];
 
-      <div class="card-body">
-        <table class="table table-bordered">
-          <thead>
-            <tr>
-              <th>S.No</th>
-              <th>Month</th>
-              <th>Fee Status</th>
-              <th>Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            <?php
-            $sql = "SELECT m.meta_value as `month` FROM `posts` as p JOIN `metadata` as m ON p.id = m.item_id WHERE p.type = 'payment' AND p.author = $user_id AND m.meta_key = 'month' AND year(p.publish_date) = 2023";
+        foreach ( $months as $key => $value )
+        {
+          $highlight = '';
 
-            $query = mysqli_query($db_conn, $sql);
-            $paid_fees = [];
+          $paid = false;
 
-            while ( $row = mysqli_fetch_object($query) )
-            {
-              $paid_fees[] = strtolower($row->month);
-            }
+          if ( in_array($value, $paid_fees) )
+          {
+            $paid = true;
+            $highlight = 'class="bg-success"';
+          }
+          // if ( date('F') == ucwords($value) )
+          // {
+          //   $highlight = 'class="bg-success"';
+          // }
+        ?>
+          <tr>
+            <td><?= ++$key; ?></td>
+            <td><?= ucwords($value); ?></td>
+            <td <?= $highlight; ?>><?= ($paid) ? "Paid" : "Pending"; ?></td>
+            <td>
+              <?php if ($paid) : ?>
+                <a href="?action=view-invoice&month=<?= $value ?>&std_id=<?= $std_id ?>" class="btn btn-sm btn-primary">
+                  <i class="fa fa-eye fa-fw"></i>
 
-            // echo '<pre>', print_r($paid_fees), '</pre>';
+                  View
+                </a>
+              <?php else : ?>
+                <a href="#" data-toggle="modal" data-month="<?= ucwords($value) ?>" data-target="#paynow-popup" class="btn btn-sm btn-warning paynow-btn">
+                  <i class="fa fa-money-check-alt fa-fw"></i>
 
-            $months = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december'];
+                  Pay Now
+                </a>
+              <?php endif; ?>
+            </td>
+          </tr>
+        <?php } ?>
+      </tbody>
+    </table>
 
-            foreach ( $months as $key => $value )
-            {
-              $highlight = '';
-
-              $paid = false;
-
-              if ( in_array($value, $paid_fees) )
-              {
-                $paid = true;
-                $highlight = 'class="bg-success"';
-              }
-              // if ( date('F') == ucwords($value) )
-              // {
-              //   $highlight = 'class="bg-success"';
-              // }
-            ?>
-              <tr>
-                <td><?= ++$key; ?></td>
-                <td><?= ucwords($value); ?></td>
-                <td <?= $highlight; ?>><?= ($paid) ? "Paid" : "Pending"; ?></td>
-                <td>
-                  <?php if ($paid) : ?>
-                    <a href="?action=view-invoice&month=<?= $value ?>&std_id=<?= $std_id ?>" class="btn btn-sm btn-primary">
-                      <i class="fa fa-eye fa-fw"></i>
-
-                      View
-                    </a>
-                  <?php else : ?>
-                    <a href="#" data-toggle="modal" data-month="<?= ucwords($value) ?>" data-target="#paynow-popup" class="btn btn-sm btn-warning paynow-btn">
-                      <i class="fa fa-money-check-alt fa-fw"></i>
-
-                      Pay Now
-                    </a>
-                  <?php endif; ?>
-                </td>
-              </tr>
-            <?php } ?>
-          </tbody>
-        </table>
-      </div>
-    </div>
   </div>
 </section>
