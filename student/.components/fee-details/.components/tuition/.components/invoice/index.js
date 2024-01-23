34b67f3a5504ba919c9ae8a62b4399ace422c652
Cassiano Castro <development.castro@gmail.com>
2024-01-23 15:28:07 -0300

    Added event listener to menu button of dialog footer component

--- student/.components/fee-details/.components/tuition/.components/invoice/index.js
+++ student/.components/fee-details/.components/tuition/.components/invoice/index.js
@@ -5,7 +5,11 @@
  */
 function index()
 {
-    //
+    const dialog = document.querySelector("#invoice");
+
+    const button = document.querySelector("#invoice footer button[title~='Print']");
+
+    button.addEventListener("click", () => window.print());
 }
 
 export { index };
