e23ea56997c64849eed60fc55e3ae139934cc159
Cassiano Castro <development.castro@gmail.com>
2024-01-23 19:07:33 -0300

    Updated include path

--- student/.components/routines/index.php
+++ student/.components/routines/index.php
@@ -1,6 +1,6 @@
-<?php require_once __DIR__ . '/periods/index.php'; ?>
+<?php require_once __DIR__ . '/.components/periods/index.php'; ?>
 
-<?php require_once __DIR__ . '/timetable/index.php'; ?>
+<?php require_once __DIR__ . '/.components/timetable/index.php'; ?>
 
 <!-- <menu>
   <li>
