938a2386f2d966dec5afed51d970df297e6b1dfe
Cassiano Castro <development.castro@gmail.com>
2024-01-24 17:36:00 -0300

    Removed quotes in SQL string param

--- includes/functions.php
+++ includes/functions.php
@@ -183,8 +183,7 @@ function get_user_metadata($user_id)
     global $db_conn;
 
     $output = [];
-
-    $query = mysqli_query($db_conn, "SELECT * FROM usermeta WHERE user_id = '$user_id'");
+    $query = mysqli_query($db_conn, "SELECT * FROM usermeta WHERE user_id = $user_id");
 
     while ( $result = mysqli_fetch_object($query) )
     {
