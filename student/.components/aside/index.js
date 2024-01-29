38cdace5b32748a4aa41db34786bc11880289258
Cassiano Castro <development.castro@gmail.com>
2024-01-29 17:07:35 -0300

    Added event listeners to HTML buttons

--- student/.components/aside/index.js
+++ student/.components/aside/index.js
@@ -5,12 +5,71 @@
  */
 function index()
 {
+    let zIndex = 0;
+
     const menu = document.querySelector("#controller menu");
 
     {
-        const button = menu.querySelector(":first-child > button");
+        const section = document.querySelector("#index");
+        const button  = menu.querySelector(":first-child > button");
 
-        button.addEventListener("click", () => location.assign('http://www.schoolsysmanager.com/student'));
+        button.addEventListener("click", () => section.style.zIndex = ++zIndex);
+    }
+
+    {
+        const section = document.querySelector("#syllabus");
+        const button = menu.querySelector(":nth-child(2) > button");
+
+        button.addEventListener("click", () => section.style.zIndex = ++zIndex);
+    }
+
+    {
+        const section = document.querySelector("#routines");
+        const button  = menu.querySelector(":nth-child(3) > button");
+
+        button.addEventListener("click", () => section.style.zIndex = ++zIndex);
+    }
+
+    {
+        const section = document.querySelector("#examinations");
+        const button  = menu.querySelector(":nth-child(4) > button");
+
+        button.addEventListener("click", () => section.style.zIndex = ++zIndex);
+    }
+
+    {
+        const section = document.querySelector("#attendance");
+        const button  = menu.querySelector(":nth-child(5) > button");
+
+        button.addEventListener("click", () => section.style.zIndex = ++zIndex);
+    }
+
+    {
+        const section = document.querySelector("#fee-details");
+        const button  = menu.querySelector(":nth-child(6) > button");
+
+        button.addEventListener("click", () => section.style.zIndex = ++zIndex);
+    }
+
+    {
+        const section = document.querySelector("#sm");
+        const button  = menu.querySelector(":nth-child(7) > button");
+
+        button.addEventListener("click", () => section.style.zIndex = ++zIndex);
+    }
+
+    {
+        const section = document.querySelector("#events");
+        const button  = menu.querySelector(":nth-child(8) > button");
+
+        button.addEventListener("click", () => section.style.zIndex = ++zIndex);
+    }
+
+    {
+        const section = document.querySelector("#meeting");
+        const button  = menu.querySelector(":nth-child(9) > button");
+
+        button.addEventListener("click", () => section.style.zIndex = ++zIndex);
     }
 }
 
